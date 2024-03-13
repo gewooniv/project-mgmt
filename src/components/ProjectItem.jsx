2e81d3077851e3794427f45cb31d35a0497cb4f8
Ivo <81419894+gewooniv@users.noreply.github.com>
2024-03-13 15:50:27 +0000

    added selection visual

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,13 +1,17 @@
-export default function ProjectItem({ project, handleSelect }) {
+export default function ProjectItem({
+  project,
+  selectedProjectId,
+  handleSelect,
+}) {
   return (
     <li>
       <button
         onClick={() => handleSelect(project.id)}
-        // className={
-        //   project.id === selectedProjectId
-        //     ? "px-4 py-2 rounded-md active: bg-stone-800"
-        //     : "px-4 py-2 rounded-md"
-        // }
+        className={
+          project.id === selectedProjectId
+            ? "px-4 py-2 rounded-md active: bg-stone-800"
+            : "px-4 py-2 rounded-md"
+        }
       >
         {project.title}
       </button>
