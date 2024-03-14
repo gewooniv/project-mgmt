cc30c4bcad4fb4423db24d2926e3a1690cd2854d
Ivo <81419894+gewooniv@users.noreply.github.com>
2024-03-14 11:30:40 +0000

    added modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
 import InputField from "./InputField";
 import Button from "./Button";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const inputTitle = useRef();
   const inputDescription = useRef();
   const inputDueDate = useRef();
@@ -12,6 +15,15 @@ export default function NewProject({ onAdd }) {
     const enteredDescription = inputDescription.current.value;
     const enteredDueDate = inputDueDate.current.value;
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -20,20 +32,30 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[30rem] px-2 py-4 text-s">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button>Cancel</button>
-        </li>
-        <li>
-          <Button onClick={handleCreate}>Save</Button>
-        </li>
-      </menu>
-      <div id="content">
-        <InputField label="title" input={inputTitle} type="text" />
-        <InputField label="description" input={inputDescription} textarea />
-        <InputField label="due date" input={inputDueDate} type="date" />
+    <>
+      <Modal ref={modal}>
+        <h2 className="my-4 text-stone-700 text-center text-3xl font-bold">
+          You have entered an invalid input
+        </h2>
+        <p className="mb-4 text-stone-600">
+          Please make sure you have provided a valid value for every input.
+        </p>
+      </Modal>
+      <div className="w-[30rem] px-2 py-4 text-s">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button>Cancel</button>
+          </li>
+          <li>
+            <Button onClick={handleCreate}>Save</Button>
+          </li>
+        </menu>
+        <div id="content">
+          <InputField label="title" input={inputTitle} type="text" />
+          <InputField label="description" input={inputDescription} textarea />
+          <InputField label="due date" input={inputDueDate} type="date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -2,13 +2,16 @@ import Button from "./Button";
 import image from "../assets/no-projects.png";
 
 export default function Content({ onCreate }) {
-    
-    return (
-        <div className="grid justify-items-center" id="content">
-            <h1 className="my-8 text-center text-3xl font-bold">Content</h1>
-            <img className="object-contain h-24" src={image} />
-            <p className="py-6">Select a project or get started on a new one</p>
-            <Button onClick={onCreate}>Create a new project</Button>
-        </div>
-    )
+  return (
+    <div className="grid justify-items-center" id="content">
+      <h1 className="my-4 text-stone-500 text-center text-3xl font-bold">
+        Content
+      </h1>
+      <img className="object-contain h-24" src={image} />
+      <p className="mb-6 text-stone-400">
+        Select a project or get started on a new one
+      </p>
+      <Button onClick={onCreate}>Create a new project</Button>
+    </div>
+  );
 }
